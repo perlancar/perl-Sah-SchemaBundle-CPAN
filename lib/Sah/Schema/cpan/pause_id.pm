8e5a3da680d89e863e9f2f0dc59c09d303f212c0
perlancar (@pc-home) <perlancar@gmail.com>
2017-01-08 14:33:18 +0700

    Initial commit

---
+++ lib/Sah/Schema/cpan/pause_id.pm
@@ -0,0 +1,14 @@
+package Sah::Schema::cpan::pause_id;
+
+# DATE
+# VERSION
+
+# CODE: require Data::Dmp; require Regexp::Pattern::CPAN; print 'our $schema = ["str", {summary=>"PAUSE author ID", match=>', Data::Dmp::dmp($Regexp::Pattern::CPAN::RE{pause_id}{pat}), '}];';
+
+1;
+
+# ABSTRACT:
+
+=head1 SEE ALSO
+
+L<Regexp::Pattern::CPAN>
---
+++ lib/Sah/Schemas/CPAN.pm
@@ -0,0 +1,13 @@
+package Sah::Schemas::CPANMeta;
+
+1;
+# ABSTRACT: Sah schemas related to CPAN
+
+=head1 SYNOPSIS
+
+
+=head1 prepend:SEE ALSO
+
+L<Sah::Schemas::CPANMeta>
+
+=cut
