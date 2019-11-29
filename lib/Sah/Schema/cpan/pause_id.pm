7b7edb606ed12a9193bfe546f04fb8e846dd0043
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2019-11-29 14:44:15 +0700

    Update coerce rule names (Data::Sah::Coerce 0.034+)

--- lib/Sah/Schema/cpan/pause_id.pm
+++ lib/Sah/Schema/cpan/pause_id.pm
@@ -1,9 +1,11 @@
 package Sah::Schema::cpan::pause_id;
 
+# AUTHORITY
 # DATE
+# DIST
 # VERSION
 
-# CODE: require Data::Dmp; require Regexp::Pattern::CPAN; my $schema = ["str", {summary=>"PAUSE author ID", match=>$Regexp::Pattern::CPAN::RE{pause_id}{pat}, 'x.perl.coerce_rules'=>['str_toupper'], 'x.completion'=>['lcpan_authorid']}, {}]; print "our \$schema = ", Data::Dmp::dmp($schema), ";\n";
+# CODE: require Data::Dmp; require Regexp::Pattern::CPAN; my $schema = ["str", {summary=>"PAUSE author ID", match=>$Regexp::Pattern::CPAN::RE{pause_id}{pat}, 'x.perl.coerce_rules'=>['From_str::to_upper'], 'x.completion'=>['lcpan_authorid']}, {}]; print "our \$schema = ", Data::Dmp::dmp($schema), ";\n";
 
 1;
 
