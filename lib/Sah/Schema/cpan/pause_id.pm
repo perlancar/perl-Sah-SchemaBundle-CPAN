a67cc7af68873ad33cf46f5610581d5891bba7db
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-03-02 02:35:10 +0700

    Fix regex: add anchoring; add examples (tests)

--- lib/Sah/Schema/cpan/pause_id.pm
+++ lib/Sah/Schema/cpan/pause_id.pm
@@ -9,10 +9,15 @@ use strict;
 use Regexp::Pattern::CPAN;
 
 our $schema = ["str", {
-    summary=>"PAUSE author ID",
-    match=>$Regexp::Pattern::CPAN::RE{pause_id}{pat},
+    summary => "PAUSE author ID",
+    match => qr/\A$Regexp::Pattern::CPAN::RE{pause_id}{pat}\z/,
     'x.perl.coerce_rules'=>['From_str::to_upper'],
     'x.completion'=>['lcpan_authorid'],
+    examples => [
+        {data=>'', valid=>0},
+        {data=>'perlancar', valid=>1, res=>'PERLANCAR'},
+        {data=>'perlancar2', valid=>0, summary=>'Too long'},
+    ],
 }, {}];
 
 1;
