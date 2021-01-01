21f4ad46bd726a5a1982e18cbae16ac8b88b244b
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-01-02 06:32:29 +0700

    Bump perl prereq to 5.014 because during build Sah::SchemaR::cpan::pause_id will contain regex sequence (?^...) [GH#1]

--- lib/Sah/Schemas/CPAN.pm
+++ lib/Sah/Schemas/CPAN.pm
@@ -1,5 +1,8 @@
 package Sah::Schemas::CPAN;
 
+# during build by perl >= 5.014, Sah::SchemaR::cpan::pause_id will contain sequence (?^...) which is not supported by perl <= 5.012
+use 5.014;
+
 1;
 # ABSTRACT: Sah schemas related to CPAN
 
